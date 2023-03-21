577c1310a327a9f12968a2c1515b6aea3d01b539
Gustavo Rodrigues Neves <06803461122@sinfra.mt.gov.br>
2023-03-21 07:32:50 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+import './App.css'
+
+var arr = new Array();
+
+function App() {  
+  const [task, SetTask] = useState([])
+
+  return (
+    
+    <div className='App'>      
+      <div>
+        <label htmlFor="name">Tarefa</label>
+        <br/>
+        <input type="text" id="name" />
+      </div>
+
+      <div>
+        <label htmlFor="date">Date</label>
+        <br/>
+        <input type="date" id="date" />
+      </div>
+
+      <div>
+        <input type="button" id="btn" value={"Adicionar"}  onClick={ ()=> {
+
+          var nameValue = document.querySelector("#name").value
+          var dateValue = document.querySelector("#date").value
+
+          dateValue = dateValue.replace(/-/g, "/")
+
+          arr.push({id: arr.length, name: nameValue, data: dateValue});
+          SetTask(new Array(arr));
+        }} />
+      </div>
+      
+      <hr />
+
+      <ul>
+        {(task.length > 0 ) ? task[0].map((t)=>(
+          <li key={t.id}>
+              <p>
+                {t.name}
+              </p>
+              -
+              <p>
+                {t.data}
+              </p>
+              <button onClick={() => {
+                debugger
+                arr.splice(arr.filter(p => p.id == t.id), 1)
+                SetTask(new Array(arr));
+              }}>
+                X
+              </button>
+          </li>
+        )) : (
+          <li>
+            <p>
+              Vazio
+            </p>
+          </li>
+          )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
